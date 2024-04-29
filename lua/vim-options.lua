7dbfaeb1c38dd2a064a8f799d7882fb6b86d1b06
DorukAytekin07 <dorukaytekinexxon@gmail.com>
2024-04-29 11:01:22 -0700

    add some neotree config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set shiftwidth=2")
 vim.cmd("set nowrap")
 
 vim.keymap.set('n','<leader>pv',vim.cmd.Ex)
